dfd51972c55f18eb1f10e396e833f01b0bf82fa1
vitekputna <vitekputna@gmail.com>
2023-10-30 20:34:37 +0100

    init

---
+++ src/mesh_manager.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+
+#define MAX_CHUNK_SIZE 8;
+
+//cache blocking
+struct mesh_chunk
+{
+    
+};
+
+//core partitions
+struct mesh_block
+{
+    
+};
+
+//array of mesh blocks (whole mesh)
+struct mesh_data
+{
+    
+};
+
+class mesh_manager
+{
+    //mesh data
+
+    //constructors
+    //destructors
+
+    //access
+    //reading and saving
+
+    //partiton mesh
+
+    //checks
+};
